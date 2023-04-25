1aa89856b5b07a5c7eb551d2b8cde4e223a5835a
reinol_martin <reinolmartin01@gmail.com>
2023-04-25 11:59:23 -0700

    new changes

---
+++ todoapp/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
+ 
+
+# Create your views here.
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse("username or password is incorrect !!")
+
+
+
+    return render(request, 'login.html')
+    
+
+
+def signup(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password')
+        my_user=User.objects.create_user(uname,email,pass1)
+        my_user.save()
+        return redirect('login')
+
+        
+        
+
+
+
+    return render(request, 'signup.html')
