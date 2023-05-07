c7ca52a3662b0444964881578a6b57f3f5e8e2f9
reinol_martin <reinolmartin01@gmail.com>
2023-05-07 15:35:20 -0700

    modified views

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def homepage(request):
     tasks = Task.objects.all()
@@ -9,11 +10,23 @@ def homepage(request):
 
 
 
+# def add_task(request):
+#     if request.method == 'POST':
+#         title = request.POST['title']
+#         description = request.POST['description']
+#         task = Task.objects.create(title=title, description=description)
+#         return redirect('tasks:home')
 def add_task(request):
     if request.method == 'POST':
-        title = request.POST['title']
-        description = request.POST['description']
-        task = Task.objects.create(title=title, description=description)
-        return redirect('tasks:home')
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('tasks:task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'add_task.html', {'form': form})
 
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
 # Create your views here.
