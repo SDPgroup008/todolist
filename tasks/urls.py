e7730e8532f32c179324e7b66f8e7818dc2972e5
reinol_martin <reinolmartin01@gmail.com>
2023-05-07 15:38:54 -0700

    modified urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('add_task/', views.add_task, name='add_task'),
+     path('', views.task_list, name='task_list'),
 ]
