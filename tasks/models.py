d471db40eb611b453842e791398700d33a970b69
reinol_martin <reinolmartin01@gmail.com>
2023-05-20 14:42:14 -0700

    modified the models

--- tasks/models.py
+++ tasks/models.py
@@ -9,4 +9,14 @@ class Task(models.Model):
     def __str__(self):
         return self.title
 
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
