69cb44c52b7b7a110d91acb3fce7bb140cddab03
reinol_martin <reinolmartin01@gmail.com>
2023-05-22 02:39:56 -0700

    modified the due date

--- tasks/models.py
+++ tasks/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    due_date = models.DateTimeField(default=timezone.now) 
+    due_date = models.DateTimeField(null=True, blank=True) 
 
     def __str__(self):
         return self.title
