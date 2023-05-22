d217e9b171aa329417dfab99fe63729a5aa55758
reinol_martin <reinolmartin01@gmail.com>
2023-05-22 02:33:32 -0700

    modified due date

--- tasks/models.py
+++ tasks/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    due_date = models.DateField()
+    due_date = models.DateTimeField(default=timezone.now) 
 
     def __str__(self):
         return self.title
