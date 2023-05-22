a5acdb0303f36720f331e041a609abc7698fa6b1
reinol_martin <reinolmartin01@gmail.com>
2023-05-22 02:17:28 -0700

    modified models

--- tasks/models.py
+++ tasks/models.py
@@ -5,6 +5,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    due_date = models.DateField()
 
     def __str__(self):
         return self.title
