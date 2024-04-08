efb2dbc19eaed141c28186bb041a6bbe2909ba9f
SDPgroup008 <reinolmartin01@gmail.com>
2024-04-08 10:26:41 -0700

    main

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) 
     title = models.CharField(max_length=200)
     description = models.TextField()
     completed = models.BooleanField(default=False)
