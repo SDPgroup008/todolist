7d991627af1b45d845b29cdd0742c7682aa8f523
SDPgroup008 <reinolmartin01@gmail.com>
2024-04-09 01:21:32 -0700

    static

--- todolist/settings.py
+++ todolist/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
 MEDIA_URL= 'images/'
@@ -131,4 +131,6 @@ MEDIA_URL= 'images/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/'
+]
